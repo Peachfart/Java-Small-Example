c8fb8729f5647b4928110cfa3c874f5e9fd3b5c5
徐同学 <xulinchun0806@outlook.com>
2021-06-29 17:29:30 +0800

    Create fourClass.java

---
+++ FirstDay/fourClass.java
@@ -0,0 +1,16 @@
+package FirstDay;
+
+import java.awt.Frame;
+
+public class fourClass {
+    public static void main(String[] args) {
+        // Fram 作用是制造串口
+        java.awt.Frame f = new Frame();
+        // 设定弹窗的宽高
+        f.setSize(800, 666);
+        // 设定弹窗的标题
+        f.setTitle("贪玩蓝月");
+        // 设定弹窗的显示方式，true是显示，false是不显示
+        f.setVisible(true);
+    }
+}
